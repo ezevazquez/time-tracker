80fc48f24f5ffa9844a232f54df11e0081932c78
ezevazquez <ezejvazquez@hotmail.com>
2025-06-27 16:47:20 -0300

    fix: add 10% assignment

--- constants/assignments.ts
+++ constants/assignments.ts
@@ -1,4 +1,4 @@
-export const ASSIGNMENT_ALLOCATION_VALUES = [0.25, 0.5, 0.75, 1] as const
+export const ASSIGNMENT_ALLOCATION_VALUES = [0.1, 0.25, 0.5, 0.75, 1] as const
 
 export const ASSIGNMENT_ALLOCATION_OPTIONS = ASSIGNMENT_ALLOCATION_VALUES.map(value => ({
   value,
