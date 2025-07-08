a3188e9f47c246ba4fb82e86a97477bd0afbd420
Cheko García <cheko.garcia@revolt.digital>
2025-07-08 08:07:09 -0600

    fix: update button label to 'Nueva asignación'

--- components/project-assignments-panel.tsx
+++ components/project-assignments-panel.tsx
@@ -47,7 +47,7 @@ export function ProjectAssignmentsPanel({
                     {showAddButton && (
                         <Button onClick={() => setShowAssignmentModal(true)} size="sm" data-test="add-assignment-button">
                             <Plus className="h-4 w-4 mr-2" />
-                            Agregar
+                            Nueva asignación
                         </Button>
                     )}
                 </div>
